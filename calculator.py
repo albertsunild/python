3d5fd4a4e6e766292a0468df278a9d3999bb25c4
Albert Sunil David <albert.sunild@hotmail.com>
2025-05-12 16:27:12 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,12 @@
+f_num = int(input("Enter a first number: "))
+s_num = int(input("Enter a second number: "))
+
+addition = (f_num + s_num)
+subtration = (f_num - s_num)
+multiplication = (f_num * s_num)
+division = (f_num/s_num)
+
+print(f"addition of {f_num} and {s_num} = {addition}")
+print(f"subtration of {f_num} and {s_num} = {subtration}")
+print(f"multiplication of {f_num} and {s_num} = {multiplication}")
+print(f"division of {f_num} and {s_num} = {division}")
